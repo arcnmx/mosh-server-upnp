69c18d73e1c966dc0345ac78078064ffeef2a72a
arcnmx <git@git.arcn.mx>
2022-08-05 08:58:42 -0700

    rust 2018

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-extern crate igd;
-extern crate get_if_addrs;
-
 use std::process::{Command, Stdio, exit};
 use std::io::{self, Write};
-use std::error::Error;
 use std::{env, net};
 use igd::SearchOptions;
 
@@ -18,7 +14,7 @@ fn addr_ip(addr: net::Ipv4Addr) -> u32 {
 fn if_for_addr(remote_addr: net::Ipv4Addr) -> io::Result<get_if_addrs::Ifv4Addr> {
     let remote_addr = addr_ip(remote_addr);
 
-    for addr in try!(get_if_addrs::get_if_addrs()) {
+    for addr in get_if_addrs::get_if_addrs()? {
         match addr.addr {
             get_if_addrs::IfAddr::V4(addr) => {
                 let ip = addr_ip(addr.ip);
@@ -35,7 +31,7 @@ fn if_for_addr(remote_addr: net::Ipv4Addr) -> io::Result<get_if_addrs::Ifv4Addr>
     Err(io::Error::new(io::ErrorKind::Other, "address not found"))
 }
 
-fn try_main() -> io::Result<i32> {
+fn main() -> io::Result<()> {
     let mosh_server = env::var_os("MOSH_SERVER");
     let mosh_server = mosh_server.as_ref().map(|s| s.as_os_str()).unwrap_or("mosh-server".as_ref());
     let mut command = Command::new(mosh_server);
@@ -46,47 +42,35 @@ fn try_main() -> io::Result<i32> {
         command.arg(arg);
     }
 
-    let output = try!(command.output());
+    let output = command.output()?;
 
     if !output.status.success() {
-        return output.status.code()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "child exited by signal"))
+        let code = output.status.code()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "child exited by signal"))?;
+        exit(code);
     }
 
-    let data = try!(String::from_utf8(output.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)));
+    let data = String::from_utf8(output.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
-    let data = try!(data.lines()
+    let data = data.lines()
         .find(|line| line.starts_with("MOSH CONNECT "))
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected connection data"))
-    );
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected connection data"))?;
 
     let mut data = data.splitn(4, ' ').skip(2);
     let data = data.next().and_then(|port| data.next().map(|secret| (port, secret)));
-    let (port, secret) = try!(data.ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "malformated connection data")));
+    let (port, secret) = data.ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "malformated connection data"))?;
 
-    let port = try!(port.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "connection port must be numeric")));
+    let port = port.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "connection port must be numeric"))?;
 
-    let gateway = try!(igd::search_gateway(SearchOptions::default())
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-    );
+    let gateway = igd::search_gateway(SearchOptions::default())
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-    let address = try!(if_for_addr(*gateway.addr.ip()));
+    let address = if_for_addr(*gateway.addr.ip())?;
     let address = net::SocketAddrV4::new(address.ip, port);
-    let external_port = try!(gateway.add_any_port(igd::PortMappingProtocol::UDP, address, 0, "mosh-server")
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-    );
+    let external_port = gateway.add_any_port(igd::PortMappingProtocol::UDP, address, 0, "mosh-server")
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-    try!(writeln!(io::stdout(), "\nMOSH CONNECT {} {}", external_port, secret));
+    writeln!(io::stdout(), "\nMOSH CONNECT {} {}", external_port, secret)?;
 
-    Ok(0)
-}
-
-fn main() {
-    match try_main() {
-        Err(err) => {
-            let _ = writeln!(io::stderr(), "mosh-server-upnp error: {}", err.description());
-            exit(1)
-        },
-        Ok(status) => exit(status),
-    }
+    Ok(())
 }
